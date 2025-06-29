9632654d099e4896730126b202bfac8198a636d0
Hosnain Rafi <rhosnain@gmail.com>
2025-06-29 10:25:30 +0600

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,42 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import jsdoc from "eslint-plugin-jsdoc";
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    plugins: {
+      jsdoc: jsdoc,
+    },
+  },
+  {
+    rules: {
+      eqeqeq: "off",
+      "no-unused-expressions": "error",
+      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+      "jsdoc/require-description": "error",
+      "jsdoc/check-values": "error",
+      "no-console": "warn",
+      "no-undef": "error",
+    },
+    globals: {
+      process: "readonly",
+    },
+  },
+  {
+    ignores: ["node_modules/**", "dist/**"],
+  },
+  tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+]);
